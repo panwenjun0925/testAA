7b5ac9a9ea15825abd56d22c38dc7b80e64aae9c
panwenjun0925 <1664222510@qq.com>
2018-11-19 17:58:00 +0800

    111

---
+++ src/main/java/com/dream/test/Test.java
@@ -0,0 +1,13 @@
+package com.dream.test;
+
+/**
+ * @Auther: PWJ
+ * @Date: 2018/11/19 17:40
+ * @Description:
+ * @Version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这是测试文件");
+    }
+}
